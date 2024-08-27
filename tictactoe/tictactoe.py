59fce3e7b3aef44b3d730a9dad836277ec7b74af
Connor Sullivan <connorssullivan25@gmail.com>
2024-08-27 09:56:51 -0400

    testing out branches

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -2,6 +2,7 @@
 Tic Tac Toe Player
 """
 
+import copy
 import math
 
 #
@@ -48,15 +49,15 @@ def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    action_list = []
+    action_list = set()
     
     #Go through the board
-    for row in len(board):
-        for cell in len(row):
+    for row in range(len(board)):
+        for col in range(len(board[row])):
 
             #If cell is empty, append the cell to action_list
-            if board[row][cell] == EMPTY:
-                action_list.append((row, cell))
+            if board[row][col] == EMPTY:
+                action_list.add((row, col))
     #Return the action_list
     return action_list
 
@@ -68,22 +69,56 @@ def result(board, action):
     Returns the board that results from making move (i, j) on the board.
     """
     #Check if the action is valid
-    if action[0] > 2 or action[1] > 2 or action[0] < 0 or action[1] < 0 or board[action[0]][action[1]] != EMPTY:
+    if action not in actions(board):
         raise Exception("Invalid Move")
     
     #Check if the player is X or O and make the move
     else:
-        new_board = board
-        player = player(board)
-        new_board[action[0]][action[1]] = player
+        row, col = action
+        new_board = copy.deepcopy(board)
+        current_player = player(board)
+        new_board[row][col] = current_player
+        return new_board
     
+#My Function
+def checkRow(board, player):
+    for row in range(len(board)):
+        if board[row][0] == player and board[row][1] == player and board[row][2] == player:
+            return True
+        
+    return False
+
+#My Function
+def checkCol(board, player):
+    for col in range(len(board)):
+        if board[0][col] == player and board[1][col] == player and board[2][col] == player:
+            return True
+        
+    return False
+
+#My Function
+def checkDiag(board, player):
+    if board[0][0] == player and board[1][1] == player and board[2][2] == player:
+        return True
+    elif board[0][2] == player and board[1][1] == player and board[2][0] == player:
+        return True
+    else: 
+        return False
+
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    
-    
+    #Check for X
+    if checkRow(board, X) or checkCol(board, X) or checkDiag(board, X):
+        return X
+    #Check for O
+    elif checkRow(board, O) or checkCol(board, O) or checkDiag(board, O):
+        return O
+    #If no winner, return None
+    else:
+        return EMPTY
         
     
 
@@ -92,18 +127,39 @@ def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    #check for winner
+    if winner(board) != EMPTY:
+        return True
+    #Check for empty cells
+    for row in board:
+        for cell in row:
+            if cell == EMPTY:
+                return False
+    return True
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    #Check for winner
+    win = winner(board)
+    if win == X:
+        return 1
+    elif win == O:
+        return -1
+    else:
+        return 0
 
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    if terminal(board):
+        return None
+
+
+
+
+
